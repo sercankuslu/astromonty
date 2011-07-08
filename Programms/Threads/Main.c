d8c3ad9670958f795a0c364545db0f34231e845c
dmitrymosh <dmitrymosh@yandex.ru>
2011-07-08 14:21:24 +0000

    чудеса SPI и ENC

--- Programms/Threads/Main.c
+++ Programms/Threads/Main.c
@@ -66,24 +66,51 @@
 #endif
 BYTE Test(BYTE a)
 {
-    BYTE i;
-    DWORD j;
+    BYTE i=173;
+    DWORD j=2020202020;
     double k=0.0;  
     double kk=0.0;   
-    for(i=0;i<50;i++){
-        for(j=1;j<360;j++)
-        {
+    /*for(i=0;i<50;i++){
+        j=i;
+        //for(j=1;j<360;j++)
+        //{*/
             k=(double)i*sin((double)j*PI/180.0);
             Nop();
             Nop();
-            kk=(double)i*sin((double)j*PI/180.0);
+            //kk=(double)i*sin((double)j*PI/180.0);
+            /*
             if(k!=kk){
                 Nop();
             }         
-        }                       
-    }
+        //}                       
+    }*/
+    LATGbits.LATG1 ^= 1;
     return 7;
 }
+BYTE Test2(BYTE a)
+{
+    BYTE i=17;
+    DWORD j=1124562;
+    double k=0.0;  
+    double kk=0.0;   
+    /*
+    for(i=0;i<50;i++){
+        j=i;
+        //for(j=1;j<360;j++)
+        //{*/
+            k=(double)i*sin((double)j*PI/180.0);
+            Nop();
+            Nop();
+            //kk=(double)i*sin((double)j*PI/180.0);
+            //if(k!=kk){
+            //    Nop();
+            //}         
+        //}                       
+    //}*/
+    LATGbits.LATG0 ^= 1;
+    return 7;
+}
+
 void Process1 (void) __attribute__ ((noreturn));
 void Process1 (void)
 {    
@@ -99,7 +126,7 @@ void Process2 (void)
     while(1){
         Nop();
         Nop();
-        Test(32);
+        Test2(32);
     }
 }
 void Process3 (void) __attribute__ ((noreturn));
@@ -123,12 +150,15 @@ void Process4 (void)
 
 void CS_On()
 {
-    LATDbits.LATD0=1;
+    LATGbits.LATG9=0;
 }
 void CS_Off()
 {
-    LATDbits.LATD0=0;
+    LATGbits.LATG9=1;
+    TRISGbits.TRISG9=0;    
 }
+
+
 //
 // Main application entry point.
 //
@@ -138,19 +168,49 @@ void main(void)
 int main(void)
 #endif
 {
-    BYTE T;
+    WORD T;
+    BYTE DevId;
+    BYTE REV=0;
     //DWORD* D;
-    T=LATAbits.LATA0;
+    CLKDIV|=0x4; //PLLPRE 4 ; PLLPOST 2
+    PLLFBD = 0x7f;
+    LATFbits.LATF0=1;
+    TRISFbits.TRISF0=0;   
+    TRISGbits.TRISG0=0;
+    TRISGbits.TRISG1=0;
+    
     
     //SPI1_Init(D);
     //CS_Control(1);
     //SPI1_Init(&CS_On,&CS_Off);
     SPI_Init();
+    DevId=SPI_RegDevice(2,2,8000000, &CS_On,&CS_Off); 
+    SPI_Open(DevId);
+    LATFbits.LATF0=1;
+    for(T=0;T<65000;T++){
+        Nop();
+        Nop();
+    }
+    //bank3 0x12; EREVID
+    //переключение на банк 3
+    //1. по адресу 1F записать 0x03
+    //команда 5F 1F
+    SPI2BUF=0x5F;
+    while ((SPI2STATbits.SPITBF == 1) || (SPI2STATbits.SPIRBF == 0));
+    SPI2BUF=0x1F;
+    while ((SPI2STATbits.SPITBF == 1) || (SPI2STATbits.SPIRBF == 0));
+    //чтение регистра
+    //2. 12 00  
+    SPI2BUF=0x12;
+    while ((SPI2STATbits.SPITBF == 1) || (SPI2STATbits.SPIRBF == 0));
+    SPI2BUF=0x00;
+    while ((SPI2STATbits.SPITBF == 1) || (SPI2STATbits.SPIRBF == 0));
+    REV = SPI2BUF;
    	InitMultiTasking();
-    StartProcess(&Process1,100);   
-    StartProcess(&Process2,100); 
-    StartProcess(&Process3,100);   
-    StartProcess(&Process4,100); 
+    StartProcess(&Process1,50);   
+    StartProcess(&Process2,50); 
+    //StartProcess(&Process3,100);   
+    //StartProcess(&Process4,100); 
     StartMultiTasking();
     //Process1();
     //Process2();
