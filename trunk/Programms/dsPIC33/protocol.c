8e0d27ad01be75b9a7c9f09eef06ddecc08fdab9
dmitrymosh@gmail.com <dmitrymosh@gmail.com@8fdf3ef5-0959-0801-4b78-0a69d592d243>
2011-10-19 18:42:37 +0000

    дщбавлена processAttribute

--- trunk/Programms/dsPIC33/protocol.c
+++ trunk/Programms/dsPIC33/protocol.c
@@ -1,5 +1,8 @@
 #include "protocol.h"
-
+typdef struct ST_CONNECTION {
+    BYTE Flag;
+}ST_CONNECTION;
+ST_CONNECTION Connection[5];
 
 BYTE FormBlob(ST_ATTRIBUTE_PTR pAttribute, BYTE ulAttribute, BYTE* Block, BYTE* ulBlockLen)
 {
@@ -18,10 +21,11 @@ BYTE FormBlob(ST_ATTRIBUTE_PTR pAttribute, BYTE ulAttribute, BYTE* Block, BYTE* 
     *ulBlockLen = BlockPtr - Block;
     return STR_OK;
 }
-BYTE ParseBlob(BYTE* Block, BYTE ulBlockLen, ST_ATTRIBUTE_PTR pAttribute, BYTE ulAttribute, BYTE** MemPtr)
+BYTE ParseBlob(BYTE* Block, BYTE ulBlockLen, ST_ATTRIBUTE_PTR pAttribute, BYTE *ulAttribute, BYTE** MemPtr)
 {
     BYTE i = 0;
-    BYTE* BlockPtr = Block;    
+    BYTE* BlockPtr = Block;
+    *ulAttribute=0;
     for(; BlockPtr < Block+ulBlockLen; i++){
         memcpy(&pAttribute[i].type, BlockPtr, sizeof(ST_ATTRIBUTE_TYPE)); 
         BlockPtr+=sizeof(ST_ATTRIBUTE_TYPE);
@@ -33,8 +37,23 @@ BYTE ParseBlob(BYTE* Block, BYTE ulBlockLen, ST_ATTRIBUTE_PTR pAttribute, BYTE u
             (*MemPtr)+=pAttribute[i].ulValueLen;
             BlockPtr+=pAttribute[i].ulValueLen;  
         } else pAttribute[i].pValue = NULL;            
+        *ulAttribute++;
     }    
 }
+BYTE ProcessAttributes(BYTE ConnectionID, BYTE* Blob, WORD* BlobLen)
+{
+    BYTE Mem[64];
+     BYTE* MemPtr = Mem;
+    memset(Mem,0,sizeof(Mem));
+     ST_ATTRIBUTE Data[10];
+    memset(Data,0,sizeof(Data));
+    BYTE ulAttributeLen=0;
+     BYTE res = 0;
+
+    res = ParseBlob(Blob, BlobLen, Data, &ulAttributeLen, &MemPtr);
+
+}
+
 
 
 
