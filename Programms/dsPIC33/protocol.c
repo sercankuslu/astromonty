77abd1d98c233a91e8716f6186818d83e0405ab6
dmitrymosh <dmitrymosh@gmail.com>
2011-10-19 15:23:58 +0000

    добавил if

--- Programms/dsPIC33/protocol.c
+++ Programms/dsPIC33/protocol.c
@@ -10,8 +10,10 @@ BYTE FormBlob(ST_ATTRIBUTE_PTR pAttribute, BYTE ulAttribute, BYTE* Block, BYTE* 
         BlockPtr+=sizeof(ST_ATTRIBUTE_TYPE);
         memcpy(BlockPtr, &pAttribute[i].ulValueLen, sizeof(WORD));
         BlockPtr+=sizeof(WORD);
-        memcpy(BlockPtr, pAttribute[i].pValue, pAttribute[i].ulValueLen);
-        BlockPtr+=pAttribute[i].ulValueLen;        
+        if(pAttribute[i].ulValueLen!=0){
+            memcpy(BlockPtr, pAttribute[i].pValue, pAttribute[i].ulValueLen);
+            BlockPtr+=pAttribute[i].ulValueLen;  
+        }      
     }
     *ulBlockLen = BlockPtr - Block;
     return STR_OK;
@@ -25,9 +27,11 @@ BYTE ParseBlob(BYTE* Block, BYTE ulBlockLen, ST_ATTRIBUTE_PTR pAttribute, BYTE u
         BlockPtr+=sizeof(ST_ATTRIBUTE_TYPE);
         memcpy(&pAttribute[i].ulValueLen, BlockPtr, sizeof(WORD));   
         BlockPtr+=sizeof(WORD);
-        memcpy((*MemPtr), BlockPtr, pAttribute[i].ulValueLen);
-        pAttribute[i].pValue = (*MemPtr);
-        (*MemPtr)+=pAttribute[i].ulValueLen;
-        BlockPtr+=pAttribute[i].ulValueLen;                
+        if(pAttribute[i].ulValueLen!=0){
+            memcpy((*MemPtr), BlockPtr, pAttribute[i].ulValueLen);
+            pAttribute[i].pValue = (*MemPtr);
+            (*MemPtr)+=pAttribute[i].ulValueLen;
+            BlockPtr+=pAttribute[i].ulValueLen;  
+        }            
     }    
 }
