948f24c264a52aa3aaa2bcf0050fd070fec615ba
dmitrymosh <dmitrymosh@yandex.ru>
2011-11-09 21:30:47 +0000

    перенес вычисления на dsPIC переключил частоту на EC w/PLL (была отключена в ENC28J60.c)

--- Programms/guidance/guidance.cpp
+++ Programms/guidance/guidance.cpp
@@ -17,7 +17,7 @@ ATOM				MyRegisterClass(HINSTANCE hInstance);
 BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
-
+ 
 
 void Calc(HDC hdc);
 double M(double F);
