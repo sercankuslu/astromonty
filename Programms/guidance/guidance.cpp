1f99a9e8229845a175f33adccb1d11350c358090
dmitrymosh <dmitrymosh@yandex.ru>
2011-11-10 12:19:45 +0000

    оптимизация+ переход на радианы

--- Programms/guidance/guidance.cpp
+++ Programms/guidance/guidance.cpp
@@ -7,6 +7,19 @@
 
 #define MAX_LOADSTRING 100
 #define PI 3.1415926535897932384626433832795
+#define ACCELERATE_SIZE 100
+#define FREQ_STEP 20
+static double Accelerate[ACCELERATE_SIZE];
+
+// усилие на валу
+static double MPower[] = {
+	0.85, 0.764642857, 0.67, 0.6, 0.53, 0.46, 0.4, 0.33, 0.22, 0.1, 0.01
+};
+// частота в Гц
+static double MaxF[] = {
+	0.0, 100.0, 250.0, 500.0, 750.0, 1000.0, 1250.0, 1500.0, 1750.0, 2000.0, 2250.0
+};
+
 // Глобальные переменные:
 HINSTANCE hInst;								// текущий экземпляр
 TCHAR szTitle[MAX_LOADSTRING];					// Текст строки заголовка
@@ -23,17 +36,30 @@ void Calc(HDC hdc);
 double M(double F);
 int SolvQuadratic(double A, double B, double C, double* X1, double* X2);
 int Calculate_dT(double Xbeg, double Xend, double V, double A, double* T);
-int Calculate_A(double V, double L, double *A);
+int Calculate_A(DWORD F, double *A);
+int InitAccelerate(double* Power, double * Freq, WORD Len, double L);
+
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
-                     HINSTANCE hPrevInstance,
-                     LPTSTR    lpCmdLine,
-                     int       nCmdShow)
+					 HINSTANCE hPrevInstance,
+					 LPTSTR    lpCmdLine,
+					 int       nCmdShow)
 {
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
- 	// TODO: разместите код здесь.
+	// TODO: разместите код здесь.
+		{
+			// I = (m*r^2/4) + (m*l^2)/12
+			static double Mass = 500.0f;
+			static double Radius = 0.30f;
+			static double Length = 2.0f;
+			static double Reduction = 360.0f;
+			static double Grad_to_Rad = 180.0/PI;
+			double I = ((Mass*Radius*Radius/4) + (Mass*Length*Length/12))/Reduction; 
+			//double L = (2 * Reduction * Grad_to_Rad)/(Mass*Radius*Radius);
+			InitAccelerate(MPower, MaxF, sizeof(MaxF)/sizeof(double), I);
+		}
 	MSG msg;
 	HACCEL hAccelTable;
 
@@ -47,7 +73,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	{
 		return FALSE;
 	}
-
+		
 	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_GUIDANCE));
 
 	// Цикл основного сообщения:
@@ -111,22 +137,22 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   HWND hWnd;
+	HWND hWnd;
 
-   hInst = hInstance; // Сохранить дескриптор экземпляра в глобальной переменной
+	hInst = hInstance; // Сохранить дескриптор экземпляра в глобальной переменной
 
-   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+	hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+	if (!hWnd)
+	{
+		return FALSE;
+	}
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+	
+	return TRUE;
 }
 
 //
@@ -165,9 +191,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		break;
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
-                //LineTo(hdc, 100,100);
+				//LineTo(hdc, 100,100);
 		// TODO: добавьте любой код отрисовки...
-                Calc(hdc);
+				Calc(hdc);
 		EndPaint(hWnd, &ps);
 
 		break;
@@ -177,7 +203,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	default:
 		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
-        
+		
 	return 0;
 }
 
@@ -203,105 +229,69 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 
 double LinInt(double x1,double y1,double x2,double y2, double x)
 {
-    if(x1!=x2) {
-        return y1+(y2-y1)*(x-x1)/(x2-x1);
-    }else return y1;
+	if(x1!=x2) {
+		return y1+(y2-y1)*(x-x1)/(x2-x1);
+	}else return y1;
 }
 void Calc(HDC hdc)
 {
-    {
-        static double Mass = 100.0f;
-        static double Radius = 2.0f;
-        double A = 0.0; //ускорение
-        double dt = 0.0; // изменение времени
-        double V = 0.0;  // мгновенная скорость
-        static double dX =1.0/(200.0*16.0);// шаг перемещения в градусах (в 1 градусе 3200 шагов)
-        double X = 0;    // полное перемещение в градусах
-        double T = 0;    // полное время
-        int K = 0;       // флаги для оптимизации вывода
-        int K1 = 0;
-        int K2 = 0;
-        int K3 = 0;
-        double timer1 = 0;  // значение таймера
-        MoveToEx(hdc, 100,0, NULL);
-        LineTo(hdc, 100,10*20);
-        MoveToEx(hdc, 0, 10*20, NULL);
-        LineTo(hdc, 1000,10*20);
-        do{
-            Calculate_A(V, 2/(Mass*Radius*Radius),&A);
-            Calculate_dT(0, dX, V, A, &dt);
-            V = dX/dt;
-            X += dX;
-            T += dt;
-            timer1 = T/0.0000002; // результат вычислений
-            K = (int)(T*500.0);
-            K2 = (int)(V*20.0);
-            if((K != K1)||(K2!=K3)) {
-                K1 = K;
-                K3 = K2;
-                 SetPixel(hdc, (int)(T*500), (int)(X*20), RGB(0,0,255));
-                 SetPixel(hdc, (int)(T*500), (int)(V*20), RGB(0,255,0));
-                 SetPixel(hdc, (int)(T*500), (int)((A)*10), RGB(255,0,0));
-                 SetPixel(hdc, (int)(T*500), (int)(timer1/100), RGB(255,0,255));
-            }
-            if(X>180.0) break;
-        }while ( V < 10);
-        //T = 0;
-        do{
-            Calculate_A(V, 2/(Mass*Radius*Radius),&A);
-            A = -A;
-            if(Calculate_dT(0, dX, V, A, &dt)!=0){
-                V=0;
-            } else
-            V = dX/dt;
-            X += dX;
-            T += dt;
-            timer1 = T/0.0000002; // результат вычислений
-            K = (int)(T*500.0);
-            K2 = (int)(V*20.0);
-            if((K != K1)||(K2!=K3)) {
-                K1 = K;
-                K3 = K2;
-                SetPixel(hdc, (int)(T*500), (int)(X*20), RGB(0,0,255));
-                SetPixel(hdc, (int)(T*500), (int)(V*20), RGB(0,255,0));
-                SetPixel(hdc, (int)(T*500), (int)((-A)*10), RGB(255,0,0));
-                SetPixel(hdc, (int)(T*500), (int)(timer1/100), RGB(255,0,255));
-            }
-            if(X<0.0) break;
-        }while ( V > 0);
-    }
+	{
+		
+		double A = 0.0; //ускорение в радианах в сек за сек
+		double dt = 0.0; // изменение времени
+		double V = 0.0;  // мгновенная скорость в радианах
+		//static double dX = 1.0/(200.0*16.0);// шаг перемещения в градусах (в 1 градусе 3200 шагов)
+		static double dX = PI/(180.0*200.0*16.0); // шаг перемещения в радианах
+		double X = 0;    // полное перемещение в радианах
+		double T = 0;    // полное время
+		int K = 0;       // флаги для оптимизации вывода
+		int K1 = 0;
+		int K2 = 0;
+		int K3 = 0;
+		double timer1 = 0;  // значение таймера
+		DWORD F = 0;
+		DWORD i = 0;
+		
+		MoveToEx(hdc, 100,0, NULL);
+		LineTo(hdc, 100,10*20);
+		MoveToEx(hdc, 0, 10*20, NULL);
+		LineTo(hdc, 500,10*20);
+		
+		do{
+			F =(DWORD)(V * 180 * 200/PI);
+			Calculate_A(F, &A);
+			Calculate_dT(0, dX, V, A, &dt);
+			V = dX/dt;
+			X += dX;
+			T += dt;
+			timer1 = T/0.0000002; // результат вычислений
+			K = (int)(T*500.0);
+			K2 = (int)(V*20.0);
+			if((K != K1)||(K2!=K3)) {
+				K1 = K;
+				K3 = K2;
+				SetPixel(hdc, (int)(T*500), (int)(X*180*20/PI), RGB(0,0,255));
+				SetPixel(hdc, (int)(T*500), (int)(V*180*20/PI), RGB(0,255,0));
+				SetPixel(hdc, (int)(T*500), (int)((1.7-A)*180*5/PI), RGB(255,0,0));
+				SetPixel(hdc, (int)(T*500), (int)(timer1/100), RGB(255,0,255));
+			}
+			//if(X>PI) break;
+			i++;
+		}while ( i< 32000);
+	}
 }
 // должна возвращать значение ускорения в зависимости от скорости
-// V - скорость в градусах в сек
+// F - частота полных шагов
 // L - момент инерции системы
-int Calculate_A(double V, double L, double *A)
+int Calculate_A(DWORD F, double *A)
 {
-    double F; // частота полных шагов
-    double R_G = 180/PI;
-    //double dX = 1.0/200; // угол в градусах полного шага
-    double Lm = 0.0;
-    // усилие на валу
-    static double MPower[] = {
-        0.85, 0.764642857, 0.67, 0.6, 0.53, 0.46, 0.4, 0.33, 0.22, 0.1, 0.01
-    };
-    // частота в Гц
-    static double MaxF[] = {
-        0.0, 100.0, 250.0, 500.0, 750.0, 1000.0, 1250.0, 1500.0, 1750.0, 2000.0, 2250.0
-    };
-    int sizeMaxF = sizeof(MaxF)/sizeof(double);
-    F = V * 200;
-    if(F > MaxF[sizeMaxF-1]){
-        Lm = MPower[sizeMaxF-1];
-    } else {
-        for(int i = 0; i< sizeMaxF-1; i++){
-            if((F >= MaxF[i]) && (F < MaxF[i+1])){
-                Lm = LinInt(MaxF[i],MPower[i],MaxF[i+1],MPower[i+1], F);
-                break;
-            }
-        }
-    }
-    *A = Lm * 360 * R_G * L;
-    return 0;
+	DWORD f = (DWORD)(F/FREQ_STEP);
+	if(f < ACCELERATE_SIZE){
+		*A = Accelerate[f];
+	} else {
+		*A = Accelerate[ACCELERATE_SIZE-1];
+	}
+	return 0;
 }
 
 // функция возвращает время, прошедшее
@@ -309,48 +299,83 @@ int Calculate_A(double V, double L, double *A)
 // при начальной скорости V и ускорении A
 int Calculate_dT(double Xbeg, double Xend, double V, double A, double* T)
 {
-    double T1 = 0.0;
-    double T2 = 0.0;
-    int res = 0;
-    if((A==0.0)&&(V==0.0)){
-        return -1; // ошибка
-    }
-    if((A == 0)&&(V != 0.0)){        
-        *T = (Xend-Xbeg)/V;
-        return 0;  // нет ошибки
-    }
-    // Xend = Xbeg + V*T + (A * T^2)/2
-    // (A/2)T^2 + V*T + (Xbeg - Xend) = 0
-    res = SolvQuadratic(A/2,V,(Xbeg-Xend),&T1, &T2); 
-    if(res > 0) {
-        *T = T1;
-        return 0;
-    }
-    else return 1;
+	double T1 = 0.0;
+	double T2 = 0.0;
+	int res = 0;
+	if((A==0.0)&&(V==0.0)){
+		return -1; // ошибка
+	}
+	if((A == 0)&&(V != 0.0)){
+		*T = (Xend-Xbeg)/V;
+		return 0;  // нет ошибки
+	}
+	// Xend = Xbeg + V*T + (A * T^2)/2
+	// (A/2)T^2 + V*T + (Xbeg - Xend) = 0
+	res = SolvQuadratic(A/2,V,(Xbeg-Xend),&T1, &T2); 
+	if(res > 0) {
+		*T = T1;
+		return 0;
+	}
+	else return 1;
 }
 // вычисляет корени квадратного уравнения
 int SolvQuadratic(double A, double B, double C, double* X1, double* X2)
 {
-    // Ax^2+Bx+C=0
-    double D = 0.0;
-    if((X1 == NULL) || (X2 == NULL)) return -1; // ошибка: неверные указатели
-    *X1 = 0.0;
-    *X2 = 0.0;
-    if(A == 0.0){
-        // Bx+C = 0
-        // x = -C/B
-        if(C!=0.0){
-            *X1 = -B/C;
-        } else {
-            *X1 = 0.0;
-        }
-        return 1; // не квадратное уравнение, один корень
-    }
-    D = B*B - 4.0 * A * C;
-    if(D<0.0) return 0; // дискриминант 0 корней нет
+	// Ax^2+Bx+C=0
+	double D = 0.0;
+	if((X1 == NULL) || (X2 == NULL)) return -1; // ошибка: неверные указатели
+	*X1 = 0.0;
+	*X2 = 0.0;
+	if(A == 0.0){
+		// Bx+C = 0
+		// x = -C/B
+		if(C!=0.0){
+			*X1 = -B/C;
+		} else {
+			*X1 = 0.0;
+		}
+		return 1; // не квадратное уравнение, один корень
+	}
+	D = B*B - 4.0 * A * C;
+	if(D<0.0) return 0; // дискриминант 0 корней нет
 
-    *X1 = (-B+sqrt(D))/(2*A);
-    *X2 = (-B-sqrt(D))/(2*A);
-    return 2; // два корня уравнения
+	*X1 = (-B+sqrt(D))/(2*A);
+	*X2 = (-B-sqrt(D))/(2*A);
+	return 2; // два корня уравнения
 }
-
+/**************************************************************************
+ * M = Ia =>   a = M/I
+ * M - момент силы
+ * I - момент инерции
+ * a - угловое ускорение в радианах в секунду за секунду
+ * для альфы: ( сплошной цилиндр длинны l, радиуса r и массы m, ось перпендикулярна целиндру и проходит через его середину)
+ * I = (m*r^2/4) + (m*l^2)/12  
+ * 
+ * для дельты: ( полый тонкостенный цилиндр длинны l, радиуса r и массы m, ось перпендикулярна целиндру и проходит через его середину)
+ * I = (m*r^2/2)+ (m*l^2/12)
+ **************************************************************************
+ * Параметры функции
+ * Power - массив моментов силы двигателя
+ * Freq  - массив частот шагов двигателя
+ * Len   - размер массивов
+ * I	 - Момент инерции  
+ * 
+ ************************************************************************/
+int InitAccelerate(double* Power, double * Freq, WORD Len, double I)
+{    
+	double Lm = 0.0;
+	int i;
+	int j;
+	WORD F = 0;
+	for(j = 0; j < ACCELERATE_SIZE; j++){
+		for(int i = 0; i< Len-1; i++){
+			if((F >= Freq[i]) && (F < Freq[i+1])){
+				Lm = LinInt(Freq[i],Power[i],Freq[i+1],Power[i+1], F);
+				break;
+			}
+		}
+		F += FREQ_STEP;
+		Accelerate[j] = Lm / I;
+	}
+	return 0;
+}
