d8132755b5f95a2e932ce52d679451ea36ac7399
dmitrymosh@yandex.ru <dmitrymosh@yandex.ru@8fdf3ef5-0959-0801-4b78-0a69d592d243>
2012-02-14 13:02:11 +0000

    Командный модуль

---
+++ trunk/Programms/PIC18F97J60/TCPIP Demo App/Control.h
@@ -0,0 +1,52 @@
+#ifndef _CONTROL_H
+#define _CONTROL_H
+
+#ifdef _WINDOWS
+#   include "..\..\guidance\stdafx.h"
+#else
+#   include "GenericTypeDefs.h"
+#endif
+
+
+typedef struct AXIS_PARAM
+{
+    BYTE  NeedToUpdate;     // необходимо запросить с сервера указанные параметры
+    BYTE  NeedToCommit;     // необходимо отправить на сервер указанные параметры
+    DWORD AbsAngle;         // текущее положение оси в милисекундах дуги
+    DWORD LogicalAngle;     // текущее направление в милисекундах времени
+    DWORD AbsSteps;         // текущий номер шага    
+} AXIS_PARAM;
+
+typedef struct NETWORK_SETTINGS
+{
+    BYTE  NeedToUpdate;     // необходимо запросить с сервера указанные параметры
+    BYTE  NeedToCommit;     // необходимо отправить на сервер указанные параметры
+    DWORD IP;
+    DWORD Mask;
+    DWORD Gate;
+    DWORD DNS1;
+    DWORD DNS2;    
+    char  Name[16];    
+}NETWORK_SETTINGS;
+
+typedef struct All_PARAMS
+{
+    // битовые карты -флаги 
+    BYTE NeedToUpdate;   // необходимо запросить с сервера указанные параметры
+    BYTE NeedToCommit;   // необходимо отправить на сервер указанные параметры
+
+    AXIS_PARAM Alpha;
+    AXIS_PARAM Delta;
+    AXIS_PARAM Gamma;
+
+    NETWORK_SETTINGS Local;
+    NETWORK_SETTINGS Remote;
+
+} ALL_PARAMS;
+
+typedef enum MENU_ID {
+    MAIN_WINDOW, MENU, SETTINGS, S_NETWORK, S_MONTY, SM_TYPESELECT, SM_ALPHA, SM_DELTA, SM_GAMMA
+} MENU_ID;
+
+void ProcessMenu( BYTE * KeyPressed);
+#endif
