4b4a953112047052c92bb3bd34a89d0d6eb46c5e
dmitrymosh@yandex.ru <dmitrymosh@yandex.ru@8fdf3ef5-0959-0801-4b78-0a69d592d243>
2012-03-20 21:05:06 +0000

    протокол работает на железном сервере + клиент в эмуляторе

--- trunk/Programms/dsPIC33/protocol.c
+++ trunk/Programms/dsPIC33/protocol.c
@@ -4,6 +4,7 @@
 #ifndef __C30__
 
 #   include "..\PIC18F97J60\TCPIP Demo App\Control.h"
+extern ALL_PARAMS Params;
 #endif
 
 #ifdef USE_PROTOCOL_CLIENT
@@ -15,7 +16,7 @@
 extern RR rr1;
 extern RR rr2;
 extern RR rr3;
-extern ALL_PARAMS Params;
+
 
 typedef struct ST_CONNECTION {
     ST_STATUS Mode;
@@ -41,75 +42,7 @@ extern DWORD_VAL CPUSPEED;
 #endif
    static BOOL ClientConnected = FALSE;
    //static BOOL ServerConnected = FALSE;
-/******************************************************************************
-*
-*
-*
-*
-*
-*
-******************************************************************************/
-ST_RESULT FormBlob(ST_ATTRIBUTE_PTR pAttribute, BYTE bAttributeLen, BYTE* pbBlock, int bBlockLen ,int* pbBlockPos)
-{
-    BYTE i = 0;  
-    int j = (*pbBlockPos);
-    for(i = 0; i < bAttributeLen; i++){
-        pbBlock[j++] = (BYTE)pAttribute[i].type;
-        pbBlock[j++] = pAttribute[i].ulValueLen;
-        if(pAttribute[i].ulValueLen!=0){
-            if(*pbBlockPos + pAttribute[i].ulValueLen > bBlockLen) 
-                return STR_BUFFER_TOO_SMALL; 
-            memcpy(&pbBlock[j], pAttribute[i].pValue, pAttribute[i].ulValueLen);
-            j += pAttribute[i].ulValueLen;
-        }
-    }
-    (*pbBlockPos) = j;
-    return STR_OK;
-}
-/******************************************************************************
-*
-*
-*
-*
-*
-*
-******************************************************************************/
-ST_RESULT ParseBlob(BYTE* pbBlock, int bBlockLen, ST_ATTRIBUTE_PTR pAttribute, BYTE *pbAttributeLen, BYTE* pbMem, BYTE bMemLen, BYTE* bMemPos)
-{
-    BYTE i = 0;    
-    //(*pbAttribute) = 0;
-    BYTE bHead;
-    BYTE j = 0;
-    bHead = sizeof(BYTE) + sizeof(BYTE);
-    for(i = 0; i < bBlockLen; ){
-        if(i + bHead > bBlockLen) return STR_DATA_CORRUPTED;
-        pAttribute[j].type = (ST_ATTRIBUTE_TYPE)pbBlock[i++];
-        pAttribute[j].ulValueLen = pbBlock[i++];
-        if(pAttribute[j].ulValueLen != 0){ 
-            if((*bMemPos + pAttribute[j].ulValueLen) > bMemLen) 
-                return STR_BUFFER_TOO_SMALL;
-            if(i + pAttribute[j].ulValueLen > bBlockLen) 
-                return STR_DATA_CORRUPTED;
-            memcpy((void*)&pbMem[*bMemPos], (void*)&pbBlock[i], pAttribute[j].ulValueLen);
-            pAttribute[j].pValue = &pbMem[*bMemPos];
-            *bMemPos += pAttribute[j].ulValueLen;
-            i += pAttribute[j].ulValueLen;
-        } 
-        else pAttribute[j].pValue = NULL;
-        *pbAttributeLen = ++j;
-        if( j >= MAX_ATTRIBUTE) 
-        	return STR_TOO_MANY_ATTRIBUTES;
-    }   
-    return STR_OK;
-}
-/******************************************************************************
-*
-*
-*
-*
-*
-*
-******************************************************************************/
+
 #ifndef __18CXX
 ST_RESULT ProtocolInit()
 {
@@ -120,25 +53,6 @@ ST_RESULT ProtocolInit()
     return STR_OK;
 }
 #endif
-/******************************************************************************
-*
-*
-*
-*
-*
-*
-******************************************************************************/
-ST_RESULT FindParam(ST_ATTRIBUTE* pData, BYTE bDataLen, ST_ATTRIBUTE_TYPE bType, BYTE * Index)
-{
-    BYTE i;
-    for(i = 0; i < bDataLen; i++) {
-        if(pData[i].type == bType){
-            (*Index) = i;
-            return STR_OK;
-        }
-    }
-    return STR_ATTR_NOT_FOUND;
-} 
 
 /******************************************************************************
 *
@@ -162,6 +76,8 @@ ST_RESULT RunServer(BYTE bConnectionID, BYTE* pbBlob, int pbBlobSize, int* pbBlo
     float d1;
     float d2;
     float d3;
+    float Target = 0.0;
+    float Speed = 0.0;
     BYTE Len = 0;
     BYTE * Answer = NULL;
     char * pLogin = NULL;
@@ -172,6 +88,7 @@ ST_RESULT RunServer(BYTE bConnectionID, BYTE* pbBlob, int pbBlobSize, int* pbBlo
     void * OutValue = NULL;
     ST_FLAGS Flag = STF_OK;
     static BYTE pbOutBlob[64];
+    RR * rr = NULL;
     
     if(*pbBlobLen == 0) return STR_OK;
     if((*pbBlobLen < 0)||(*pbBlobLen >= pbBlobSize)) {
@@ -305,55 +222,44 @@ ST_RESULT RunServer(BYTE bConnectionID, BYTE* pbBlob, int pbBlobSize, int* pbBlo
             case STC_SEND_DATA:
                 res = STR_OK;
                 break;
-#ifdef _DISABLE_
             case STC_EXECUTE_COMMAND: // *************************************************************************
                 {
-                    for(j = 0; j < bAttributeLen; j++){
-                        switch (Data[j].type){
-                        case STA_ALPHA_START:
-                            {
-                                res = FindParam(Data, bAttributeLen, STA_ALPHA_TARGET, &k);
-                                if(res != STR_OK) {
-                                    break;
-                                }
-                                memcpy(&d1, Data[k].pValue, Data[k].ulValueLen);
-                                // TODO: искать скорость наведения в атрибутах
-                                GoToCmd(&rr1, 1.0 * Grad_to_Rad, d1, 0);                                                               
-                                Answers = STF_OK;
-                            }
-                            break;
-                        case STA_DELTA_START:
-                            {
-                                res = FindParam(Data, bAttributeLen, STA_DELTA_TARGET, &k);
-                                if(res != STR_OK) {
-                                    break;
-                                }
-                                memcpy(&d2, Data[k].pValue, Data[k].ulValueLen);
-
-                                GoToCmd(&rr2, 1.0 * Grad_to_Rad, d2, 0);                                                               
-                                Answers = STF_OK;
-                            }
-                            break;
-                        case STA_GAMMA_START:
-                            {
-                                res = FindParam(Data, bAttributeLen, STA_GAMMA_TARGET, &k);
-                                if(res != STR_OK) {
-                                    break;
-                                }
-                                memcpy(&d3, Data[k].pValue, Data[k].ulValueLen);
-                                GoToCmd(&rr3, 1.0 * Grad_to_Rad, d3, 0);                                                               
-                                Answers = STF_OK;
-                            }
-                            break;
-                        default:;
-
-                        };
+                    pPointer = NULL;
+                    res = BeginCommand(pbOutBlob, sizeof(pbOutBlob), STC_EXECUTE_ANSWER);
+                    if(res != STR_OK){
+                        Answers = STF_DATA_ERROR;
+                        Connections[bConnectionID].Mode = STS_NO_CONNECT;
+                        break;
                     }
-                         
-                }                
+                    Target = 0.0;
+                    Speed = 0.0;
+                    do{              
+                        res = GetNextAttribute(pbBlob, *pbBlobLen, &pPointer, &Attribute, &Len, (BYTE**)&Value);
+                        switch (Attribute){
+                        case STA_ALPHA:
+                            rr = &rr1;                                                       
+                            break;
+                        case STA_DELTA:
+                            rr = &rr2;                            
+                            break;
+                        case STA_GAMMA:
+                            rr = &rr3;                            
+                            break;
+                        case STA_TARGET:
+                            Target = *((float*)Value);
+                            break;
+                        case STA_SPEED:
+                            Speed = *((float*)Value);
+                            break;
+                        case STA_START:
+                            GoToCmd(rr, Speed, Target, 0); 
+                            break;
+                        default: break;
+                        }                        
+                    } while(res != STR_ATTR_NOT_FOUND);
+                }              
                 res = STR_OK;
                 break;
-#endif
             default :
                 res = BeginCommand(pbOutBlob, sizeof(pbOutBlob), STC_NO_COMMANDS);
                 if(res != STR_OK){
@@ -428,21 +334,24 @@ ST_RESULT  RunClient(BYTE* pbBlob, int bBlobLen, int *pbDataLength)
         ClientConnected = FALSE;
         return STR_NEED_DISCONNECT;
     }
-    res = CheckBlob(pbBlob, bBlobLen);
-    if(res != STR_OK) {
-        ST_STATE = ST_REQUEST_CONNECT;
-        ClientConnected = FALSE;
-        return STR_NEED_DISCONNECT;
-    }
-    res = FindAttribute(pbBlob, pbBlobLen, STA_COMMAND, &Len, &Answer);
-    if(res != STR_OK){
-        ST_STATE = ST_REQUEST_CONNECT;
-        ClientConnected = FALSE;       
-        return STR_COMMAND_UNKNOWN;
-    }
-    if(Len == sizeof(BYTE)){
-        Command = (ST_COMMANDS)*Answer;  
-    }
+    if(*pbDataLength > 0){        
+        res = CheckBlob(pbBlob, bBlobLen);
+        if(res != STR_OK) {
+            ST_STATE = ST_REQUEST_CONNECT;
+            ClientConnected = FALSE;
+            return STR_NEED_DISCONNECT;
+        }
+        res = FindAttribute(pbBlob, bBlobLen, STA_COMMAND, &Len, &Answer);
+        if(res != STR_OK){
+            ST_STATE = ST_REQUEST_CONNECT;
+            ClientConnected = FALSE;       
+            return STR_COMMAND_UNKNOWN;
+        }
+        if(Len == sizeof(BYTE)){
+            Command = (ST_COMMANDS)*Answer;  
+        }
+    } else 
+        Command = STC_NO_COMMANDS;
    
     while(DoWork){
         DoWork = FALSE;
@@ -455,7 +364,10 @@ ST_RESULT  RunClient(BYTE* pbBlob, int bBlobLen, int *pbDataLength)
             ST_STATE = ST_WAIT_CONNECT;
             break;
         case ST_WAIT_CONNECT:
-            if(*pbDataLength==0) break;            
+            if(Command != STC_CONNECT_ANSWER){
+                ST_STATE = ST_REQUEST_CONNECT;
+                return STR_NEED_DISCONNECT;
+            }
             res = FindAttribute(pbBlob, bBlobLen, STA_FLAG, &Len, &Answer);
             if(res != STR_OK){
                 ST_STATE = ST_REQUEST_CONNECT;
@@ -496,9 +408,12 @@ ST_RESULT  RunClient(BYTE* pbBlob, int bBlobLen, int *pbDataLength)
             }
             res = STR_NEED_ANSWER; 
             ST_STATE = ST_WAIT_AUTH;
-            brea;
+            break;
         case ST_WAIT_AUTH:
-            if(*pbDataLength==0) break;            
+            if(Command != STC_AUTH_ANSWER){
+                ST_STATE = ST_REQUEST_CONNECT;
+                return STR_NEED_DISCONNECT;
+            }
             res = FindAttribute(pbBlob, bBlobLen, STA_FLAG, &Len, &Answer);
             if(res != STR_OK){
                 ST_STATE = ST_REQUEST_CONNECT;
@@ -541,12 +456,80 @@ ST_RESULT  RunClient(BYTE* pbBlob, int bBlobLen, int *pbDataLength)
                         }
                     }
                 } while (res != STR_ATTR_NOT_FOUND);
+                if(res == STR_ATTR_NOT_FOUND) res = STR_OK;
                 break;
-            default:;
+            case STC_CONNECT_ANSWER:
+            case STC_AUTH_ANSWER:
+                ST_STATE = ST_REQUEST_CONNECT;
+                return STR_NEED_DISCONNECT;
+                break;
+            default: ;
             }
-            
-            // отправка данных 
-            // все переменные, полученные из блоба должны быть сохранены в других местах!!!
+            // отправка команд
+            if(Params.NeedToCommit.Val > 0){
+                res = BeginCommand(pbBlob, bBlobLen, STC_EXECUTE_COMMAND);
+                if(res != STR_OK){
+                    ST_STATE = ST_REQUEST_CONNECT;
+                    break;
+                }
+                if(Params.NeedToCommit.bits.Alpha){
+                    if(Params.Alpha.NeedToCommit.bits.TargetAngle){
+                        // выбираем канал ALPHA
+                        res = AddAttribute(pbBlob, bBlobLen, STA_ALPHA, 0, &bNull); 
+                        if(res != STR_OK){
+                            ST_STATE = ST_REQUEST_CONNECT;
+                            break;
+                        }
+                        Value = &Params.Alpha.TargetAngle;
+                        Params.Alpha.NeedToCommit.bits.TargetAngle = 0;
+                        Params.NeedToCommit.bits.Alpha = 0;
+                    }
+                } else
+                if(Params.NeedToCommit.bits.Delta){
+                    if(Params.Delta.NeedToCommit.bits.TargetAngle){
+                        // выбираем канал ALPHA
+                        res = AddAttribute(pbBlob, bBlobLen, STA_DELTA, 0, &bNull); 
+                        if(res != STR_OK){
+                            ST_STATE = ST_REQUEST_CONNECT;
+                            break;
+                        }
+                        Value = &Params.Delta.TargetAngle;
+                        res = STR_NEED_ANSWER; 
+                        Params.Delta.NeedToCommit.bits.TargetAngle = 0;
+                        Params.NeedToCommit.bits.Delta = 0;
+                    }
+                } else
+                if(Params.NeedToCommit.bits.Gamma){
+                    if(Params.Gamma.NeedToCommit.bits.TargetAngle){
+                        // выбираем канал ALPHA
+                        res = AddAttribute(pbBlob, bBlobLen, STA_GAMMA, 0, &bNull); 
+                        if(res != STR_OK){
+                            ST_STATE = ST_REQUEST_CONNECT;
+                            break;
+                        }
+                        Value = &Params.Gamma.TargetAngle;
+                        res = STR_NEED_ANSWER; 
+                        Params.Gamma.NeedToCommit.bits.TargetAngle = 0;
+                        Params.NeedToCommit.bits.Gamma = 0;
+                    }
+                }
+                // задаем координаты
+                res = AddAttribute(pbBlob, bBlobLen, STA_TARGET, (BYTE)sizeof(float), (BYTE*)Value);
+                if(res != STR_OK){
+                    ST_STATE = ST_REQUEST_CONNECT;
+                    break;
+                }
+                // подтверждаем начало выполнения
+                res = AddAttribute(pbBlob, bBlobLen, STA_START, 0, &bNull);
+                if(res != STR_OK){
+                    ST_STATE = ST_REQUEST_CONNECT;
+                    break;
+                }
+                res = STR_NEED_ANSWER;
+                break;
+            }
+            // отправка запросов на данные 
+            // все переменные, полученные из блоба должны быть сохранены в других местах!!!            
             if(Params.NeedToUpdate.Val > 0){
                 res = BeginCommand(pbBlob, bBlobLen, STC_REQEST_DATA);
                 if(res != STR_OK){
@@ -612,35 +595,13 @@ ST_RESULT  RunClient(BYTE* pbBlob, int bBlobLen, int *pbDataLength)
             return res;
         }
         res = GetCommandLength(pbBlob, bBlobLen, pbDataLength);
+        res = STR_NEED_ANSWER;
     } else {
         *pbDataLength = 0;
     }
     return res;
 }
 #endif // USE_PROTOCOL_CLIENT
-
-ST_RESULT  CopyAttribute(ST_ATTRIBUTE pDest, ST_ATTRIBUTE pSource, BYTE *pbMem, BYTE bMemLen, BYTE* bMemPos )
-{
-        pDest.type = pSource.type;
-        pDest.ulValueLen = pSource.ulValueLen;
-        
-        if((*bMemPos + pDest.ulValueLen) > bMemLen) 
-                return STR_BUFFER_TOO_SMALL;	
-        memcpy(&pbMem[*bMemPos], pSource.pValue, pSource.ulValueLen);
-        pDest.pValue = &pbMem[*bMemPos];
-        *bMemPos += pDest.ulValueLen;
-        return STR_OK;
-}
-
-BOOL IsClientConnected()
-{
-    return ClientConnected;
-}
-
-void SetClientDisconnect()
-{
-    ClientConnected = FALSE;
-}
 
 ST_RESULT BeginCommand(BYTE * pBuffer, int bBufLen, ST_COMMANDS Value)
 {
@@ -664,7 +625,7 @@ ST_RESULT AddAttribute(BYTE * pBuffer, int bBufLen, ST_ATTRIBUTE_TYPE Attribute,
     *pPointer++ = (BYTE)bValueLen;    
     memcpy(pPointer, Value, bValueLen);
     // в первом байте хранится размер всего блока
-    pBuffer[0] += sizeof(BYTE(Attribute)) + sizeof((BYTE)bValueLen) + bValueLen;
+    pBuffer[0] += sizeof((BYTE)Attribute) + sizeof((BYTE)bValueLen) + bValueLen;
     return STR_OK;
 }
 ST_RESULT CheckBlob(BYTE * pBuffer, int bBufLen)
@@ -723,80 +684,7 @@ ST_RESULT GetNextAttribute(BYTE * pBuffer, int bBufLen, BYTE ** pPointer, ST_ATT
     return STR_OK;
 }
 
-
-#ifdef __DISABLE__
-void ExecuteCommands()
+ST_RESULT SetClientDisconnect()
 {
-    static BYTE datareq = STC_REQEST_DATA;
-    static BYTE execreq = STC_EXECUTE_COMMAND;
-    static ST_ATTRIBUTE RequestData[] = {
-        {STA_COMMAND,  sizeof(BYTE), &datareq},
-        {STA_NULL,  0, NULL},
-    };
-    static ST_ATTRIBUTE Data = {
-        STA_NULL,  0, NULL
-    };
-    
-    if(Params.NeedToCommit.Val > 0){
-        if(Params.NeedToCommit.bits.Alpha){
-            if(Params.Alpha.NeedToCommit.bits.TargetAngle){
-                RequestData[0].pValue = &execreq;
-                rv = PushAttr(RequestData[0], OUT_BUFFER);
-                if(rv!=RB_OK) return;
-                RequestData[1].type = STA_ALPHA_START;
-                RequestData[1].pValue = NULL;
-                RequestData[1].ulValueLen = 0;
-                rv = PushAttr(RequestData[1], OUT_BUFFER);
-                if(rv!=RB_OK) return;
-                RequestData[1].type = STA_ALPHA_TARGET;
-                RequestData[1].pValue = &Params.Alpha.TargetAngle;
-                RequestData[1].ulValueLen = sizeof(float);
-                rv = PushAttr(RequestData[1], OUT_BUFFER);
-                if(rv!=RB_OK) return;
-                Params.Alpha.NeedToCommit.bits.TargetAngle = 0;
-            }
-        }
-        if(Params.NeedToCommit.bits.Delta){
-            if(Params.Delta.NeedToCommit.bits.TargetAngle){
-                RequestData[0].pValue = &execreq;
-                rv = PushAttr(RequestData[0], OUT_BUFFER);
-                if(rv!=RB_OK) return;
-                RequestData[1].type = STA_DELTA_START;
-                RequestData[1].pValue = NULL;
-                RequestData[1].ulValueLen = 0;
-                rv = PushAttr(RequestData[1], OUT_BUFFER);
-                if(rv!=RB_OK) return;
-                RequestData[1].type = STA_DELTA_TARGET;
-                RequestData[1].pValue = &Params.Delta.TargetAngle;
-                RequestData[1].ulValueLen = sizeof(float);
-                rv = PushAttr(RequestData[1], OUT_BUFFER);
-                if(rv!=RB_OK) return;
-                Params.Delta.NeedToCommit.bits.TargetAngle = 0;
-            }
-        }
-        if(Params.NeedToCommit.bits.Gamma){
-            if(Params.Gamma.NeedToCommit.bits.TargetAngle){
-                RequestData[0].pValue = &execreq;
-                rv = PushAttr(RequestData[0], OUT_BUFFER);
-                if(rv!=RB_OK) return;
-                RequestData[1].type = STA_GAMMA_START;
-                RequestData[1].pValue = NULL;
-                RequestData[1].ulValueLen = 0;
-                rv = PushAttr(RequestData[1], OUT_BUFFER);
-                if(rv!=RB_OK) return;
-                RequestData[1].type = STA_GAMMA_TARGET;
-                RequestData[1].pValue = &Params.Gamma.TargetAngle;
-                RequestData[1].ulValueLen = sizeof(float);
-                rv = PushAttr(RequestData[1], OUT_BUFFER);
-                if(rv!=RB_OK) return;
-                Params.Gamma.NeedToCommit.bits.TargetAngle = 0;
-            }
-        }
-        Params.NeedToCommit.bits.Alpha = 0;
-        Params.NeedToCommit.bits.Delta = 0;
-        Params.NeedToCommit.bits.Gamma = 0;
-    } else
-        
-
+    return STR_OK;
 }
-#endif //#ifdef __DISABLE__
