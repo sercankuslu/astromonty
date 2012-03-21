77d83f56a4dd549da929390f7ec7805a71080643
dmitrymosh <dmitrymosh@yandex.ru>
2012-03-21 19:32:49 +0000

    забытые файлы

---
+++ Programms/PIC18F97J60_test/TCPIP Demo App/font.h
@@ -0,0 +1,28 @@
+#ifndef _FONT_H
+#define _FONT_H
+
+#ifdef _WINDOWS
+#   include "..\..\guidance\stdafx.h"
+#else
+#   include "GenericTypeDefs.h"
+#endif
+
+typedef enum FONT {
+    ARIAL_L, 
+    ARIAL_B
+} FONT;
+
+#define ARIAL_B_MASK 0x03FF
+#define ARIAL_MASK 0x00FF
+#define SIZE_ARIAL_B 10
+#define SIZE_ARIAL 8
+
+int GetSymbolImage(BYTE symbol, WORD* Image, WORD* ImageSize, FONT CFont);
+WORD* GetStringImage(BYTE* string, WORD* OutWordCount);
+
+ 
+
+//#define ARIAL_B  0   //Arial_B
+//#define ARIAL    0   //Arial_B
+
+#endif //_FONT_H
