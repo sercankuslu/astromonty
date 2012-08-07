3cd57746349e5509a11eb53ff20cced67e002f00
dmitrymosh@yandex.ru <dmitrymosh@yandex.ru@8fdf3ef5-0959-0801-4b78-0a69d592d243>
2012-08-07 15:04:00 +0000

    новый вид для сайта+ динамическая загрузка JS + подсказка с координатами

--- trunk/Programms/dsPIC33/TCPIP Demo App/MainDemo.c
+++ trunk/Programms/dsPIC33/TCPIP Demo App/MainDemo.c
@@ -429,7 +429,8 @@ int main(void)
 
 	
  	
-	OCInit();/*
+	//OCInit();
+	/*
     PushCmdToQueue(&rr1, ST_ACCELERATE, 20.0 * Grad_to_Rad, 180.0 * Grad_to_Rad, 1);
     PushCmdToQueue(&rr1, ST_RUN, 20.0,  90.0 * Grad_to_Rad, 1);
     PushCmdToQueue(&rr1, ST_DECELERATE, 0.0 * Grad_to_Rad, 180.0 * Grad_to_Rad, 1); 
@@ -463,9 +464,9 @@ int main(void)
     // down into smaller pieces so that other tasks can have CPU time.
     while(1)
     {
-        Control(&rr1);
-	    Control(&rr2);
-		Control(&rr3);
+        //Control(&rr1);
+	    //Control(&rr2);
+		//Control(&rr3);
         // Blink LED0 (right most one) every second.
         
         //if(TickGet() - d >= TICK_SECOND/16000ul)
@@ -474,8 +475,8 @@ int main(void)
         //   	LATDbits.LATD0 ^= 1; // выход STEP
             
         //}
-        TimerMonitor();
-        FanControl();		
+        //TimerMonitor();
+        //FanControl();		
 		if(PORTAbits.RA13 != t){
 		    t = PORTAbits.RA13;
 	            LED0_IO = t;
