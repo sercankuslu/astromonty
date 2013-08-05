d7c414180047d8977f413b3d9c287269c9dfac76
dmitrymosh@gmail.com <dmitrymosh@gmail.com@8fdf3ef5-0959-0801-4b78-0a69d592d243>
2013-08-05 04:19:49 +0000

    uFS++

--- trunk/Programms/FileSystem/FileSystem.h
+++ trunk/Programms/FileSystem/FileSystem.h
@@ -3,30 +3,39 @@
 #include "resource.h"
 
 #define MAX_FILE_NAME_LENGTH 32
+#define uFS_SEEK_SET 0	// Beginning of file
+#define uFS_SEEK_CUR 1	// Current position of the file pointer
+#define uFS_SEEK_END 2	// End of file 
+//#define uFS_MODE_READ 1
+//#define uFS_MODE_WRITE 2
+//#define uFS_MODE_APPEND 3
+//#define uFS_MODE_NORMAL 0
+//#define uFS_MODE_EXTENDED 1
 
-/*
- *        создать, если нет
- *        | удалить файл, если есть/не удалять файл
- *        | | текстовый(0)/бинарный(1)
- *        | | | запись в конец/запись по указателю
- *        | | | | чтение разрешено
- *        | | | | | запись разрешена
- *        | | | | | |
- *   |7|6|5|4|3|2|1|0|
- */
+enum ModeEnum
+{
+    uFS_MODE_NONE = 0, 
+    uFS_MODE_READ, 
+    uFS_MODE_WRITE, 
+    uFS_MODE_APPEND,
+    uFS_MODE_READ_PLUS, 
+    uFS_MODE_WRITE_PLUS, 
+    uFS_MODE_APPEND_PLUS
+};
+enum BinaryEnum { uFS_TEXT = 0, uFS_BINARY};
+enum DirEnum { uFS_RECORD_IS_FILE = 0, uFS_RECORD_IS_DIR};
+
 typedef union {
     BYTE Val;
     //BYTE_VAL VAL;
     struct {
-        unsigned RE:1;
-        unsigned WE:1;
-        unsigned WrToEnd:1;
-        unsigned Binary:1;
-        unsigned Renew:1;
-        unsigned New:1;
-        unsigned b6:1;
-        unsigned b7:1;
-    } bits;
+        ModeEnum Mode:4; // r/w/a = 01/10/11        
+        unsigned bit4:1;
+        unsigned bit5:1;
+        BinaryEnum Binary:1;
+        DirEnum Dir:1;        
+    } Enums;
+       
 }FILESTATE;
 
 typedef struct _INODE_RECORD
@@ -62,9 +71,10 @@ typedef struct _uFS_FILE
 
 int uFS_Init();
 int CreateFS();
-uFS_FILE *  FS_fopen ( const char * filename, const char * mode );
+uFS_FILE *  uFS_fopen ( const char * filename, const char * mode );
+int uFS_fclose( uFS_FILE * stream);
 int uFS_fwrite ( BYTE * ptr, DWORD size, DWORD count, uFS_FILE * stream );
 int uFS_fread ( BYTE * ptr, DWORD size, DWORD count, uFS_FILE * stream );
 int uFS_fseek ( uFS_FILE * stream, int offset, int origin );
-
+int uFS_remove ( const char * filename );
 
